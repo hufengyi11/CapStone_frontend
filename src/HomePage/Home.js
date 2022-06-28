9819ea56b25cd2398b0a6193a052c984e0a42a27
hufengyi11 <fengyihu456@gmail.com>
2022-06-28 16:56:57 +0100

    made small changes to logo

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -23,7 +23,7 @@ export default function Home() {
             <Fruits />
             <Meat />
             <Toiletries />
-            <Wellbeing />
+            {/* <Wellbeing /> */}
             <Footer />
         </>
 
