0e8f05eaff056df01fd7d55cfd283df9ab92aca7
hufengyi11 <fengyihu456@gmail.com>
2022-06-28 10:54:17 +0100

    adding item detail page

---
+++ src/HomePage/ItemList/ItemList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Vegetable from '../VegetableList/Vegetable.js'
+import './ItemList.css'
+
+export default function ItemList() {
+  return (
+    <div>
+        <Vegetable />
+    </div>
+  )
+}
