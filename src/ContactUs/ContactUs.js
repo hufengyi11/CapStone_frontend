159208000109b933ad3307beb497236e8cfdc3ba
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-06-29 11:57:47 +0100

    contacts form updated

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -1,37 +1,47 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com'
 
-const ContactUs = () => {
-    const navigate = useNavigate;
-    return(
 
-        <>
-        <h3>Contact Us</h3>
-      <p>If you have any enquiries or issues, please do not hesitate to drop a message below!</p>
-            <form className="form">
-                <label className="formName">Name:</label>
-                <input 
-                    type="name" 
-                    id="name" 
-                    placeholder="E.g. Mohamed Ismail"
-                />
-                <label className="formEmail">Email:</label>
-                <input 
-                    type="email" 
-                    id="email" 
-                    placeholder="E.g. mohamedismail@email.co.uk"
-                 />
-                <label className="formEnquiry">Enquiry:</label>
-                <input 
-                    id="enquiry"
-                    placeholder="Please describe your issue to us..."
-                >
-                    </input> 
+const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
 
-                <input type="submit" value="Submit" />
-            </form>
-        </>
+    const submit = () => {
+        if (name && email && message) {
+            const serviceId = 'service_y7l234l';
+            const templateId = 'template_s5rvmqg';
+            const userId = 'SZcn2mHqjBK2Ky7Aq';
+            const templateParams = {
+                name,
+                email,
+                message
+            };
+
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error));
+
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert('Please fill in all fields.');
+        }
+    }
+
+    return (
+        <div id="contact-form">
+            <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
+            <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
+            <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+            <button onClick={submit}>Send Message</button>
+
+            <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
+        </div>
     );
-}
+};
 
-export default ContactUs;
+export default Contact;
