d764a90f741c0860e33638230dd843c0ff325ec4
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-06-29 21:25:42 +0100

    starting categories section

--- src/FirstHomePage/Categories.js
+++ src/FirstHomePage/Categories.js
@@ -6,9 +6,12 @@ const Categories = () => {
     const navigate = useNavigate();
     return(
         <>
+        <div className="categorytitle">
+        <h2 className="title">Browse a popular category</h2>
+        <p className="shopping">Start Shopping <button className="shoppingbutton" onClick={() => {navigate('shopping', {replace:true})}}><FaArrowRight /></button> </p>
+        </div>
         <div>
-        <h2>Browse a popular category</h2>
-        <p>Start Shopping <button onClick={() => {navigate('shopping', {replace:true})}}><FaArrowRight /></button> </p>
+            
         </div>
         </>
     );
