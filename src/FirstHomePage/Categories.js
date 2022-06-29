d2268d9c9685e56983aaa35feac8bd0fb022a9ae
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-06-29 21:57:40 +0100

    added icons and boxes to categories

--- src/FirstHomePage/Categories.js
+++ src/FirstHomePage/Categories.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaBreadSlice, FaTablets, FaToiletPaper } from "react-icons/fa";
+import { GiMilkCarton, GiSodaCan, GiMeat, GiFruitBowl, GiBroccoli } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
 
 const Categories = () => {
@@ -10,9 +11,42 @@ const Categories = () => {
         <h2 className="title">Browse a popular category</h2>
         <p className="shopping">Start Shopping <button className="shoppingbutton" onClick={() => {navigate('shopping', {replace:true})}}><FaArrowRight /></button> </p>
         </div>
-        <div>
-            
-        </div>
+        <div className="categorycontainer1">
+            <div className="category">
+                <h3>Dairy</h3>
+                <div><GiMilkCarton /></div>
+            </div>
+            <div className="category">
+                <h3>Drink</h3>
+                <div><GiSodaCan /></div>
+            </div>
+            <div className="category">
+                <h3>Bakery</h3>
+                <div><FaBreadSlice /></div>
+            </div>
+            <div className="category">
+                <h3>Toiletries</h3>
+                <div><FaToiletPaper /></div>
+            </div>
+            </div>
+            <div className="categorycontainer2">
+            <div className="category">
+                <h3>Wellbeing</h3>
+                <div><FaTablets /></div>
+            </div>
+            <div className="category">
+                <h3>Meat</h3>
+                <div><GiMeat /></div>
+            </div>
+            <div className="category">
+                <h3>Vegetable</h3>
+                <div><GiBroccoli /></div>
+            </div>
+            <div className="category"> 
+                <h3>Fruit</h3>
+                <div><GiFruitBowl /></div>
+            </div>
+            </div>
         </>
     );
 }
