1b9be8e63efb083df752c6e0b3b86f51910656dc
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-06-30 13:17:14 +0100

    faqs

--- src/FAQs/Faqs.js
+++ src/FAQs/Faqs.js
@@ -1,66 +1,134 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import './Faqs.css'
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
 
 const Faqs= () =>{
     
     return(
         <>
-            <h1>GoMart FAQs</h1>
-
-                <button className="accordion"> <h2> What is GoMart?</h2></button>
-                <div className="panel"> 
-                    <p> 
-                        Groceries delivered to your doorstep within minutes
-                    </p>
-                </div>
-
-            <button className="accordion">How much does GoMart cost?</button>
-            <div className="panel">
-                    <p>
-                        Delivery fees range from £1.00 to £2.00 depending on your area.
-                    </p>
+            <h1 className="names">GoMart FAQs</h1>
+            <div>
+                <Accordion className="accordion">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content">
+                    <Typography>
+                        What is GoMart?
+                    </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>         
+                            Groceries delivered to your doorstep within minutes
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
             </div>
-
-                <button className="accordion">What is the minimum payment</button>
-                <div className="panel">
-                    <p>
-                        Our minimum order value can range from £3 to £5 depending on the store.
-                    </p>
-                </div>
-
-
-                <button className="accordion">How late does GoMart deliver?</button>
-                <div className="panel">
-                    <p>
-                        The stores will have different closing times so do check their
-                        info out!
-                    </p>
-                </div>
-            
-                <button className="accordion">Can I pay by cash?</button>
-                <div className="panel">
-                    <p>
-                        Due to COVID-19, we currently do not accept cash payments in order to offer
-                        contactless delivery.
-                    </p>
-                </div>
-
-                <button className="accordion">Where can I write a review?</button>
-                <div className="panel">
-                    <p>
+            <div>
+                <Accordion className="accordion">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content">
+                    <Typography>
+                        How much does GoMart cost?
+                    </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>         
+                            Delivery fees range from £1.00 to £2.00 depending on your area.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+            <div>
+                <Accordion className="accordion">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content">
+                    <Typography>
+                        What is the minimum payment
+                    </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>         
+                            Our minimum order value can range from £3 to £5 depending on the store.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+            <div>
+                <Accordion className="accordion">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content">
+                    <Typography>
+                        How late does GoMart deliver?
+                    </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>         
+                            The stores will have different closing times so do check their
+                            info out!
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+            <div>
+                <Accordion className="accordion">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content">
+                    <Typography>
+                        Can I pay by cash?
+                    </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>         
+                            Due to COVID-19, we currently do not accept cash payments in order to offer
+                            contactless delivery.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+            <div>
+                <Accordion className="accordion">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content">
+                    <Typography>
+                        Where can I write a review?
+                    </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>         
                         If you would like to let us know how we're doing, follow the link on the bottom
                         of the page under 'Support'.
-                    </p>
-                </div>
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+            <div>
+                <Accordion className="accordion">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content">
+                    <Typography>
+                        My order was cancelled but I still see a charge on my account. What happens?
+                    </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>         
+                            Orders usually take 3 to 5 business days to clear the account, so expect a full 
+                            refund by that time.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
 
-                <button className="accordion">My order was cancelled but I still see a charge on my account. What happens?</button>
-                <div className="panel"> 
-                    <p>
-                        Orders usually take 3 to 5 business days to clear the account, so expect a full 
-                        refund by that time.
-                    </p>
-                </div>
             <Footer />        
         </>
     );
