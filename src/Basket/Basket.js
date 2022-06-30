657f6ea816bd2c77288aa92a4ead71c234a2da89
hufengyi11 <fengyihu456@gmail.com>
2022-06-30 16:24:04 +0100

    making basket

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Basket.css'
 import Footer from "../Footer/Footer";
 
 const Basket = () => {
-    return(
+    const [basketItems, setBasketItems] = useState([]);
+
+    const addToBasket = (item) => {
+
+        setBasketItems([...basketItems, item])
+    }
+
+    return (
         <>
-            <h1>
-                hi
-            </h1>
+            <h1>Your Basket</h1>
 
-
+            <ul>
+                {basketItems}
+            </ul>
 
             <Footer />
         </>
--- src/HomePage/Bakery.js
+++ src/HomePage/Bakery.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import './Item.css'
 
-export default function Bakery() {
+export default function Bakery({handleClick}) {
     const [items, setItems] = useState([]);
+    const [addToBasketItem, setAddToBasketItem] = useState();
 
     useEffect(() => {
         axios.get('http://localhost:8080/items/bakery')
@@ -12,6 +13,8 @@ export default function Bakery() {
                 setItems(items);
             }).catch((err) => console.log(err));
     }, []);
+
+    
 
     return (
         <div className='ItemDisplay'>
@@ -22,7 +25,7 @@ export default function Bakery() {
                         <div key={item.id} className='ItemGroup'>
                             <div className='ItemCard'></div>
                             {`${item.name}`}
-                            <button>Add to cart</button>
+                            <button onClick={() => handleClick(item)}>Add to cart</button>
                         </div>
                     )
                 }
