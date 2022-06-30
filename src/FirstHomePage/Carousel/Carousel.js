ae3bc6daa5f2b5d8fd07ed21f3cd2eba68fd2007
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-06-30 01:24:16 +0100

    trying to start carousel

---
+++ src/FirstHomePage/Carousel/Carousel.js
@@ -0,0 +1,15 @@
+
+export const Carousel = [
+    {
+        title: "bread",
+        urls: "src/Images/Fresh_made_bread_05.jpeg"
+    },
+    {
+        title: "milk",
+        urls: "src/Images/3_5144_W300H400_Milk-Bottles_53e7fb.jpeg"
+    },
+    {
+        title: "eggs",
+        urls: "src/Images/uncover-hidden-egg-ingredients-1324275-primary-recirc-3cf777cca7044ee1992cc0a27d6449fa.webp"
+    },
+];
--- src/FirstHomePage/EnterAddress.js
+++ src/FirstHomePage/EnterAddress.js
@@ -3,6 +3,8 @@ import './FirstHomePage.css';
 import { Button } from "@material-ui/core";
 import { FaArrowRight } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+
+
 const EnterAdress = () => {
 
     const navigate = useNavigate();
@@ -17,8 +19,12 @@ const EnterAdress = () => {
             <Button><button type="submit" className="button" onClick={() => {navigate('shopping', {replace:true})}}><FaArrowRight /></button>
             </Button>
             </div>
+            
+                
+            <div className="imagecontainer">
             <div className="flexbox-item-2">image carousel</div>
             <div className="flexbox-item-3">image carousel</div>
+            </div>
         </div>
         </>
     );
