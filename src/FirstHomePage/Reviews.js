3414d0bfa817a8b6b729ae194d4aaa2642a67562
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-07-01 12:07:50 +0100

    reviews added

---
+++ src/FirstHomePage/Reviews.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Reviews = () => {
+    return(
+        <>
+        <div>
+        <img className="reviews" src={require("/Users/mohamedismail/coursework/week_12/day_1/CapStone_frontend/src/Images/reviews.png")}  alt="reviews"/>
+        </div>
+        </>
+    );
+}
+
+export default Reviews;
