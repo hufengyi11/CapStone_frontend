a1ab2731f7a7232124c0853d61c6ce730e3470fe
hufengyi11 <fengyihu456@gmail.com>
2022-07-05 12:43:46 +0100

    restructure

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -22,84 +22,6 @@ export default function Home() {
             }).catch((err) => console.log(err));
     }, [])
 
-<<<<<<< Updated upstream
-    useEffect(() => {
-        axios.get('http://localhost:8080/items')
-            .then(res => {
-                const items = res.data;
-                setItems(items);
-            }).catch((err) => console.log(err));
-    }, [])
-
-    const [items, setItems] = useState([]);
-    const [filteredItems, setFilteredItems] = useState();
-    const [basketItem, setBasketItem] = useState([]);
-
-    const ItemCard = ({ id, name, price, rating, imagepath, handelAddToCard }) => {
-        return (
-            <div className='itemcontainerwithcart'>
-                <div className='itemcontainer'>
-                    <img className='itemimage' src={`${imagepath}.jpeg`}/>
-                </div>
-                <h2 className='itemtitle'>{name}</h2>
-                <p>£{price}</p>
-                <p>{rating} star</p>
-                <button className='AddtocartButton' onClick={() => handelAddToCard(id)}>+</button>
-            </div>
-        )
-    }
-
-    const ItemList = ({ items, handelAddToCard }) => {
-        let mappedshoppingItem = items.map(item => {
-            return (
-                <ItemCard
-                    name={item.name}
-                    price={item.price}
-                    rating={item.rating}
-                    key={item.id}
-                    id={item.id}
-                    imagepath={item.imagepath}
-                    handelAddToCard={handelAddToCard}
-                />
-            )
-        })
-
-        return (
-            <div className='ItemListContainer'>
-                <div className='ItemCardContainer'>{mappedshoppingItem}</div>
-            </div>
-        )
-    }
-
-    const BasketItemCard = ({ name, price }) => {
-        return (
-            <div className='BasketItemCard'>
-                <h2>{name}</h2>
-                <h4>{price}</h4>
-            </div>
-        )
-    }
-
-    const BasketList = ({ items }) => {
-        let mappedBasketItem = items.map(item => {
-            return (
-                <BasketItemCard
-                    name={item.name}
-                    price={item.price}
-                />
-            )
-        })
-        return(
-            <div className='BasketItemList'>{mappedBasketItem}</div>
-        )
-    }
-
-    function AddToCart(input) {
-        const purchasingItem = items.find(item => item.id == input);
-        setBasketItem([...basketItem,purchasingItem])
-    }
-
-=======
     const [items, setItems] = useState([]);
     const [filteredItems, setFilteredItems] = useState();
     const [basketItem, setBasketItem] = useState([]);
@@ -117,7 +39,6 @@ export default function Home() {
         setBasketItem([...basketItem,purchasingItem])
     }
 
->>>>>>> Stashed changes
     function searchFunction(searchInput) {
         const filtered = items.filter(item => item.name.toLowerCase().includes(searchInput.toLowerCase()));
         setFilteredItems(filtered);
