f7294e9ea5b04399ca7d8baaceb9e3276d5bc90e
hufengyi11 <fengyihu456@gmail.com>
2022-07-06 15:44:22 +0100

    added basket items

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -4,50 +4,55 @@ import Layout from '../Layout/Layout'
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
 import BasketContext from '../BasketContext/BasketContext';
+import './Basket.css'
+import { Remove } from '@material-ui/icons';
 
 function Basket() {
     const { basketItems, setBasketItems } = useContext(BasketContext);
 
-    const BasketItemCard = ({ name, price, RemoveFromBasket }) => {
-        return (
-            <div className='BasketItemCard'>
-                <h3>{name}</h3>
-                <h3>£{price}</h3>
-                <button className='removebutton' onClick={() => RemoveFromBasket(name)}>X</button>
+    const uniqueNames = basketItems.map(item => item.id)
+        .filter((value, index, self) => self.indexOf(value) === index)
+        .map(i =>
+            <div className='basketListContainer'>
+                <div className='basketListItem'>
+                    <h3>{basketItems.filter(p => p.id == i)[0].name}</h3>
+                    <h4>price: £{basketItems.filter(p => p.id == i)[0].price}</h4>
+                </div>
+                <h4 className='basketListItemCount'>
+
+                    {/* <button className='button' >-</button> */}
+
+                    amount: {basketItems.filter(p => p.id == i).length}
+
+                    {/* <button className='button' onClick={Add(i)}>+</button> */}
+                </h4>
             </div>
         )
-    }
-    const BasketList = ({ RemoveFromBasket }) => {
-        let mappedBasketItem = basketItems.map(item => {
-            return (
-                <BasketItemCard
-                    name={item.name}
-                    price={item.price}
-                    RemoveFromBasket={RemoveFromBasket}
-                    key={item.id}
-                />
-            )
-        })
-        return (
-            <div className='BasketItemList'>{mappedBasketItem}</div>
-        )
-    }
 
-    function RemoveFromCart(input) {
-        const cartItems = basketItems.slice().filter((a) => a.name !== input);
-        setBasketItems(cartItems)
-    }
+    const priceList = basketItems.map(k => k.price)
+    let orderValue = priceList.reduce((a, b) => a + b, 0)
+    const totalValue = orderValue + 2
+
+
+
+    // function RemoveFromCart(input) {
+    //     const cartItems = basketItems.slice().filter((a) => a.name !== input);
+    //     setBasketItems(cartItems)
+    // }
 
     return (
         <div>
             <Layout />
-            <div className='BasketContainer'>
-                <h1>Basket</h1>
-                <p>You have {basketItems.length} items in the basket.</p>
-            </div>
-            <BasketList RemoveFromBasket={RemoveFromCart}/>
-            <div className='padding'></div>
+            <h1 className='title'>Shopping Bag</h1>
 
+            <div className='BasketContainer'>
+                {uniqueNames}
+                <h3>Order Value: £{orderValue}</h3>
+                <h3>Delivery Cost: £2</h3>
+                <h2>Total: £{totalValue}</h2>
+            </div>
+
+            <div className='padding'></div>
             <Footer />
         </div>
     )
