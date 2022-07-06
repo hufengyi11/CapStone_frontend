2f0b615365eda7c73ad99a4b75007d05ff014b41
hufengyi11 <fengyihu456@gmail.com>
2022-07-06 11:55:00 +0100

    useContext implemented

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -1,26 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Footer from '../Footer/Footer'
 import Layout from '../Layout/Layout'
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
+import BasketContext from '../BasketContext/BasketContext';
 
 function Basket() {
-    const navigate = useNavigate;
-    const [basketItem, setBasketItem] = useState([]);
+    const { basketItems, setBasketItems } = useContext(BasketContext);
+
+    const BasketItemCard = ({ name, price, RemoveFromBasket }) => {
+        return (
+            <div className='BasketItemCard'>
+                <h3>{name}</h3>
+                <h3>£{price}</h3>
+                <button className='removebutton' onClick={() => RemoveFromBasket(name)}>X</button>
+            </div>
+        )
+    }
+    const BasketList = ({ RemoveFromBasket }) => {
+        let mappedBasketItem = basketItems.map(item => {
+            return (
+                <BasketItemCard
+                    name={item.name}
+                    price={item.price}
+                    RemoveFromBasket={RemoveFromBasket}
+                    key={item.id}
+                />
+            )
+        })
+        return (
+            <div className='BasketItemList'>{mappedBasketItem}</div>
+        )
+    }
+
+    function RemoveFromCart(input) {
+        const cartItems = basketItems.slice().filter((a) => a.name !== input);
+        setBasketItems(cartItems)
+    }
+
     return (
         <div>
             <Layout />
-            {/* <div className='BasketContainer'>
-            
-            <BasketList items={basketItem} RemoveFromBasket={RemoveFromCart} />
-            </div> */}
             <div className='BasketContainer'>
-            <h1>Basket</h1>
-            <p>You have {basketItem.length} items in the basket.</p>
-                <h2>Your basket</h2>
-                <p>Item in cart: 0</p>
-                <p>Total price: 0</p>
+                <h1>Basket</h1>
+                <p>You have {basketItems.length} items in the basket.</p>
             </div>
+            <BasketList RemoveFromBasket={RemoveFromCart}/>
             <div className='padding'></div>
 
             <Footer />
